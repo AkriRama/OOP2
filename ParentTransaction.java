47391e26bc492662ba7bd96c78932cfe804da802
AkriR <114977574+AkriRama@users.noreply.github.com>
2024-12-03 16:55:34 +0700

    Update Transactions.java

--- ParentTransaction.java
+++ ParentTransaction.java
@@ -1,8 +1,7 @@
 package day5.Tugas;
 
-public abstract class ParentTransaction {
+public abstract class Transactions {
     private int[] id;
-    private String[] name;
 
     public int[] getId() {
         return id;
@@ -10,14 +9,6 @@ public abstract class ParentTransaction {
 
     public void setId(int[] id) {
         this.id = id;
-    }
-
-    public String[] getName() {
-        return name;
-    }
-
-    public void setName(String[] name) {
-        this.name = name;
     }
 
     public void display(String name) {
