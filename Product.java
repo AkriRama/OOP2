61eb5b150bd9eb83b853c095ef194b43fb046e2a
AkriR <114977574+AkriRama@users.noreply.github.com>
2024-12-03 16:56:23 +0700

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,8 +1,20 @@
 package day5.Tugas;
 
-public class Product extends ParentTransaction {
-    private int[] stock;
-    private Float[] price;
+import day5.Tugas.repository.IndexInterface;
+import day5.Tugas.utils.FormatCurrency;
+
+public class Product extends Transactions implements IndexInterface {
+    private String[] name = new String[100];
+    private int[] stock = new int[100];
+    private Float[] price = new Float[100];
+
+    public String[] getName() {
+        return name;
+    }
+
+    public void setName(String[] name) {
+        this.name = name;
+    }
 
     public int[] getStock() {
         return stock;
@@ -20,34 +32,19 @@ public class Product extends ParentTransaction {
         this.price = price;
     }
 
-    @Override
-    public int[] getId() {
-        return super.getId();
-    }
-
-    @Override
-    public String[] getName() {
-        return super.getName();
-    }
-
-    @Override
-    public void setId(int[] id) {
-        super.setId(id);
-    }
-
-    @Override
-    public void setName(String[] name) {
-        super.setName(name);
-    }
-
     public void display(String name) {
         super.display(name);
-        System.out.println("Name\t\tStock\tPrice");
-        for (int i = 0; i < getName().length; i++) {
-            if (getStock()[i] > 0) {
-                System.out.println(
-                        i + 1 + ". " + getName()[i] + "\t" + getStock()[i] + "\t" + getPrice()[i]);
+        if (getIndex() > 0) {
+            System.out.println("Name\t\tStock\tPrice");
+            for (int i = 0; i < getName().length; i++) {
+                if (getStock()[i] > 0) {
+                    System.out.println(
+                            i + 1 + ". " + getName()[i] + "\t" + getStock()[i] + "\t"
+                                    + FormatCurrency.setFormatCurrency(getPrice()[i]));
+                }
             }
+        } else {
+            System.out.println("No Product");
         }
     }
 
@@ -71,6 +68,33 @@ public class Product extends ParentTransaction {
             }
         }
         setStock(currentStock);
+    }
+
+    @Override
+    public int getIndex() {
+        int n = 0;
+        for (String i : getName()) {
+            if (i == null) {
+                return n;
+            }
+            n++;
+        }
+        return 1;
+    }
+
+    public void addProduct(String name, int stock, Float price) {
+        String[] listName = getName();
+        int[] listStock = getStock();
+        Float[] listPrice = getPrice();
+
+        int lastProduct = getIndex();
+        listName[lastProduct] = name;
+        listStock[lastProduct] = stock;
+        listPrice[lastProduct] = price;
+
+        setName(listName);
+        setStock(listStock);
+        setPrice(listPrice);
     }
 
 }
